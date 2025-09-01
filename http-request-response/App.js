bd9039624785060f9f402d61b329cb2a52a76c8c
Bakhtiar Kasi <bakhtiarkasi@gmail.com>
2025-09-01 20:54:29 +0500

    The http request response app with solution

--- http-request-response/App.js
+++ http-request-response/App.js
@@ -10,6 +10,35 @@ import {
 
 export default App = () => {
   const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMenuData = async () => {
+    try {
+      const response = await fetch(
+        'https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu-items-by-category.json'
+      );
+      const jsonMenu = await response.json();
+      setData(jsonMenu.menu);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMenuData();
+  });
+
+  const Item = ({ name, price }) => (
+    <View style={menuStyles.menuContainer}>
+      <Text style={menuStyles.menuItem}>{name}</Text>
+       <Text style={menuStyles.menuItem}>${price}</Text>
+    </View>
+  );
+
+  const renderListItem = ({ item }) => <Item name={item.title} price={item.price} />;
 
   return (
     <SafeAreaView style={menuStyles.container}>
@@ -17,9 +46,10 @@ export default App = () => {
       {isLoading ? (
         <ActivityIndicator />
       ) : (
-        {
-          /* Render flatlist with menu items after fetching from API  here*/
-        }
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={renderListItem}></FlatList>
       )}
     </SafeAreaView>
   );
@@ -30,8 +60,20 @@ const menuStyles = StyleSheet.create({
     flex: 1,
   },
   headerText: {
-    color: '#495E57',
+    color: '#f0c423',
     fontSize: 30,
     textAlign: 'center',
   },
+  menuContainer:{
+    flex:0.9,
+    backgroundColor: '#485c58',
+    flexDirection:'row',
+    justifyContent: 'space-between'
+  },
+  menuItem:{
+    color: '#f0c423',
+    fontSize: 24,
+    textAlign: 'left',
+    padding:30
+  }
 });
